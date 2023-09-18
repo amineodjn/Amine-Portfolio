123b54ff15c2ab734524d02d892a14b873385ebf
Amine Oudjana <amine.oudjana@withsecure.com>
2023-09-18 19:10:23 +0200

    styled the h tags and add the svg

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,4 +1,11 @@
 <template>
+    <header class="header">
+        <nav>
+            <a href="/">amineodj</a>
+            <a href=""><img src="/home/amineoudjana/AminePortfolio/AminePortfolio/AminePortfolio/images/icon-github.svg" alt="GitHub"></a>
+        </nav>
+
+    </header>
 <div class="wrapper">
 <h1>Nice to meet you! I'm Amine Oudjana.</h1>
 </div>
